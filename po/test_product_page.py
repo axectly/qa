ec13ec4438f2988cd9b9d0b3b71ef209af793b5a
axectly <amurxrus13@gmail.com>
2021-11-03 12:02:26 +0300

    add new testcase

--- po/test_product_page.py
+++ po/test_product_page.py
@@ -1,13 +1,25 @@
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
+import pytest
 
 import time
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = MainPage(browser, link)
-    page.open()
-    page = ProductPage(browser, browser.current_url)
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
+# def test_guest_can_add_product_to_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page = ProductPage(browser, browser.current_url)
+#     page.add_product_to_basket()
+#     page.solve_quiz_and_get_code()
     
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  8, 9])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_message_about_adding()
+    page.should_be_message_basket_total()
